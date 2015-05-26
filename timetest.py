f50178884e7713c0ff722671d98297b11429e2a7
Sergey Romanov <xxsmotur@gmail.com>
2015-05-26 14:16:16 +0500

    add platform information

--- timetest.py
+++ timetest.py
@@ -1,5 +1,6 @@
 import datetime
-
+import sys
+import psutil
 
 #Main Event object
 class Event:
@@ -19,6 +20,7 @@ class TimeTest:
     def __init__(self, backend=None):
         self.events = []
 
+
     def __call__(self, fn, *args, **kwargs):
         self._construct_event(fn, fn.__name__)
         def inner(*args, **kwargs):
@@ -29,6 +31,9 @@ class TimeTest:
         expected = kwargs.get('expected',0)
         hardlimit = kwargs.get('hardlimit',0)
         self.events.append(Event(fn, name, expected_delta=expected, hardlimit=hardlimit))
+
+    def _platform_info(self):
+        return {'platform': sys.platform, 'cpucount': psutil.cpu_count(), 'pyversion': sys.version_info, 'memory': psutil.virtual_memory()}
 
     def addTest(self, title, fn, *args, **kwargs):
         self._construct_event(fn,title, *args,**kwargs)
@@ -41,6 +46,16 @@ class TimeTest:
 
     def run(self):
         report = []
+        platform_item = self._platform_info()
+        print("Platform information:\n")
+        print("OS: {0}".format(platform_item['platform']))
+        print("CPU count: {0}".format(platform_item['cpucount']))
+        pyversion = platform_item['pyversion']
+        print("Python version: {0}.{1}".format(pyversion.major, pyversion.minor))
+        memory = platform_item['memory']
+        print("Total virtual memory: {0}".format(memory.total))
+        print("Available virtual memory: {0}\n".format(memory.available))
+
         for event in self.events:
             eventstart = datetime.datetime.now()
             event.fn()
