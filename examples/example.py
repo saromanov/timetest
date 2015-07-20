7fc2c916690d63ea6a5a43de109dfec3d5418368
Sergey Romanov <xxsmotur@gmail.com>
2015-07-21 00:23:48 +0500

    update example

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,6 @@
 from timetest import TimeTest
 import numpy as np
+import time
 
 app = TimeTest('timetest')
 
@@ -14,5 +15,9 @@ def dotproduct():
     A = np.random.random((10000,100))
     B = np.random.random((10000,100))
     np.dot(A.T,B)
+
+@app
+def timer():
+    time.sleep(2)
 
 app.run()
